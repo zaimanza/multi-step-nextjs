9972c52ced87354d78de1d99dd1267c4dce2e5a3
MANZA <36519974+zaimanza@users.noreply.github.com>
2022-03-29 14:04:59 +0800

    30:34

---
+++ components/steps/Final.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Final() {
+    return (
+        <div>Final</div>
+    )
+}
+
+export default Final
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    './pages/**/*.{js,jsx}',
+    './components/**/*.{js,jsx}'
+  ],
+  theme: {
+    extend: {
+      spacing: {
+        '2xl': '480px',
+        '9xl': '60px',
+      },
+    },
+  },
+  plugins: [],
+};
